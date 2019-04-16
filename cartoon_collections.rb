fe91e5aab69c08f03d3156e2ef8a8ec9eb72ea3e
Erin Stalzer <erinstalzer@gmail.com>
2019-04-16 23:00:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   final_calls
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
 end
 
